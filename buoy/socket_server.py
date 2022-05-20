7a1b5ae698b6248ebf856c38d0d326ef2f69b040
doc2kim <81610276+doc2kim@users.noreply.github.com>
2022-05-20 15:19:32 +0900

    aa

--- buoy/socket_server.py
+++ buoy/socket_server.py
@@ -4,7 +4,7 @@ import socket, time, threading
 
 def binder(client_socket, addr):
     # sys.path.append('../')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     import django
     django.setup()
     from buoy.models import Buoy, Location, Data 
