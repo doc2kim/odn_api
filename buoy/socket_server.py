271e4bcb89c9c2cd0541456c3c95463767dfd4e7
doc2kim <81610276+doc2kim@users.noreply.github.com>
2022-05-21 20:39:22 +0900

    aa

--- buoy/socket_server.py
+++ buoy/socket_server.py
@@ -3,10 +3,11 @@ import sys
 import socket, time, threading
 
 def binder(client_socket, addr):
-    # sys.path.append('../')
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-    # import django
-    # django.setup()
+    # sys.path.append("../")
+    sys.path.append("/var/app/venv/staging-LQM1lest/lib/python3.8/site-packages")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    import django
+    django.setup()
     
     now = time
     try:
