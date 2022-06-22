90ffb30180227d702fbd01a382a97406d414a103
doc2kim <81610276+doc2kim@users.noreply.github.com>
2022-06-23 03:07:50 +0900

    aa

--- buoy/models.py
+++ buoy/models.py
@@ -6,7 +6,7 @@ from django_filters import DateFromToRangeFilter, FilterSet,  TimeRangeFilter, A
 
 class Buoy(models.Model):
     id = models.IntegerField(
-        help_text="Buoy ID", primary_key=True)
+        help_text="Buoy ID",  primary_key=True)
     voltage = models.FloatField(null=True)
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Buoy(models.Model):
 
 class Coordinate(models.Model):
     buoy_id = models.ForeignKey(
-        Buoy, related_name="coordinate", on_delete=models.PROTECT, db_column="buoy_id")
+        Buoy, related_name="coordinate", on_delete=models.PROTECT, null=True, db_column="buoy_id_id")
     lat = models.FloatField()
     lon = models.FloatField()
 
@@ -25,7 +25,7 @@ class Coordinate(models.Model):
 
 class MeasureTime(models.Model):
     coordinate = models.ForeignKey(
-        Coordinate, related_name="measure_time", on_delete=models.PROTECT, db_column="coordinate_id")
+        Coordinate, related_name="measure_time", on_delete=models.PROTECT, null=True, db_column="coordinate_id")
     date = models.DateField()
     time = models.TimeField()
 
@@ -35,7 +35,7 @@ class MeasureTime(models.Model):
 
 class Measure(models.Model):
     measure_time = models.ForeignKey(
-        MeasureTime, related_name="measure", on_delete=models.PROTECT, db_column="measure_time_id")
+        MeasureTime, related_name="measure", on_delete=models.PROTECT, null=True, db_column="measure_time_id")
     temp = models.FloatField()
     oxy = models.FloatField()
     ph = models.FloatField()
